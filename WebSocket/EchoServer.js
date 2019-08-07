7c0a94db9a2ac1dacbc8363190dd142c6d8336e1
Dong-wook94 <gygacpu@naver.com>
2019-08-07 16:33:34 +0900

    창문제어 추가 wiznet contest 제출용

--- WebSocket/EchoServer.js
+++ WebSocket/EchoServer.js
@@ -1,17 +1,18 @@
 ﻿#!/usr/bin/env node
 var WebSocketServer = require('websocket').server;
 var http = require('http');
-var Client=[];
- 
-var server = http.createServer(function(request, response) {
+var Client = [];
+var Webos_conn="";
+var Window_conn="";
+var server = http.createServer(function (request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
-server.listen(8000, function() {
+server.listen(8000, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
- 
+
 wsServer = new WebSocketServer({
     httpServer: server,
     // You should not use autoAcceptConnections for production
@@ -21,41 +22,64 @@ wsServer = new WebSocketServer({
     // to accept it.
     autoAcceptConnections: false
 });
- 
+
 function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
 }
 
 
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
     }
- 
-    var flag =0;
+
+    var flag = 0;
     var connection = request.accept(null, request.origin);
-    for(i=0;i<Client.length;i++){
-        if(Client[i].remoteAddress==connection.remoteAddress){
+    if (connection.remoteAddress == "::ffff:155.230.29.14") {
+        Webos_conn = connection;
+        console.log("webos 서버 connected");
+        //Webos_conn.sendUTF("명훈이 받음?");
+    }
+    if (connection.remoteAddress == "::ffff:155.230.29.19") {
+        Window_conn = connection;
+        console.log("창문제어 서버 connected");
+        //Webos_conn.sendUTF("명훈이 받음?");
+    }
+    for (i = 0; i < Client.length; i++) {
+        if (Client[i].remoteAddress == connection.remoteAddress) {
             flag = 1;
             connection = Client[i];
         }
     }
-    if(flag==0){
+    if (flag == 0) {
         Client.push(connection);
-        connection = Client[Client.length-1];
+        connection = Client[Client.length - 1];
     }
     //var connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data+"address : "+ connection.remoteAddress);
-            console.log("Client 수 :"+Client.length);
-            console.log("Client:"+Client);
-            connection.sendUTF("서버 지기쥬("+connection.remoteAddress+") :"+message.utf8Data);
+            console.log('Received Message: ' + message.utf8Data + "address : " + connection.remoteAddress);
+            console.log("Client 수 :" + Client.length);
+            console.log("Client:" + Client);
+            //console.log("서버에서 보낸 메시지 : "+"서버(" + connection.remoteAddress + ") :" + message.utf8Data);
+            //connection.sendUTF("서버(" + connection.remoteAddress + ") :" + message.utf8Data);
+            //connection.sendUTF(message.utf8Data);
+            if (Webos_conn!="") {
+               // connection.sendUTF("2명훈이 받음?");
+               console.log("web으로 전송 : "+ message.utf8Data);
+               Webos_conn.sendUTF(message.utf8Data);
+            }
+            if (Window_conn!="") {
+                // connection.sendUTF("2명훈이 받음?");
+                console.log("창문서버로 전송 : "+ message.utf8Data);
+                Window_conn.sendUTF(message.utf8Data);
+             }
+            //Webos_conn.sendUTF(message.utf8Data)
         }
 
         else if (message.type === 'binary') {
@@ -63,7 +87,7 @@ wsServer.on('request', function(request) {
             connection.sendBytes(message.binaryData);
         }
     });
-    connection.on('close', function(reasonCode, description) {
+    connection.on('close', function (reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
